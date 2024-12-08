0f08b9499d9f776bcf18b15273fee6af6c1554f4
aodeyn <aodeyn@gmail.com>
2024-12-08 17:42:16 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -5,7 +5,7 @@ class House:
      #   n = int(number_of_floors)
     def go_to(self,new_floor):
         for i in range(int(new_floor)):
-            if 0 < int(new_floor) and int(new_floor) < int(self.number_of_floors):
+            if 0 < int(new_floor) and int(new_floor) <= int(self.number_of_floors):
                 print(i+1)
             else:
                 print("Такого этажа не существует.")
