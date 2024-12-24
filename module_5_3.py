aecb5a4fdd597fae349f232ce18df829f40b789f
aodeyn <aodeyn@gmail.com>
2024-12-24 10:12:50 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -13,24 +13,36 @@ class House:
         return self.number_of_floors
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors} '
-    def __eq__(self,other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        def __eq__(self,other):
+        if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
     def __lt__(self,other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
     def __le__(self,other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
     def __gt__(self,other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
     def __ge__(self,other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
     def __ne__(self,other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+        if isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
     def __add__(self, value):
         self.number_of_floors += value
         return self
